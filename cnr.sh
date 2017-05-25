0adff74a33457a9c42f75691586e65484e3edbf3
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2017-05-25 14:02:04 -0400

    cnr.sh: add download_or_noop function
    This should cause the first attmept to use the registry plugin to
    download the latest binary for the current platform.

--- cnr.sh
+++ cnr.sh
@@ -1,72 +1,85 @@
 #!/bin/bash
+#set -e
 
-function update_cnr {
-    echo "wip: install/update cnr-cli"
+LATEST="v0.4.0"
 
-
-
-    if [ -e $HELM_PLUGIN_DIR/cnr ] ; then
-        chmod +x $HELM_PLUGIN_DIR/cnr
+function download_or_noop {
+  if [ ! -e "$HELM_PLUGIN_DIR/cnr" ]; then
+    local PLATFORM="linux"
+    if [ "$(uname)" = "Darwin" ]; then
+      PLATFORM="osx"
     fi
 
+    local URL="https://github.com/app-registry/helm-plugin/releases/download/$LATEST/registry-helm-plugin-$LATEST-$PLATFORM-x64.tar.gz"
+
+    if which curl > /dev/null; then
+      curl -s -L $URL | tar xf - registry/cnr
+    else
+      wget -q -O - $URL | tar xf - registry/cnr
+    fi
+
+    mv registry/cnr "$HELM_PLUGIN_DIR/cnr"
+    rm -rf registry
+    chmod +x "$HELM_PLUGIN_DIR/cnr"
+  fi
 }
 
 function pull {
-    #echo "pull $@"
-    release=`$HELM_PLUGIN_DIR/cnr pull --media-type helm ${@} |tail -n1`
-    echo $release
+  #echo "pull $@"
+  release=$($HELM_PLUGIN_DIR/cnr pull --media-type helm ${@} | tail -n1)
+  echo "$release"
 }
 
 function install {
-    $HELM_PLUGIN_DIR/cnr helm install $@
+  $HELM_PLUGIN_DIR/cnr helm install $@
 }
 
 function dep {
-    $HELM_PLUGIN_DIR/cnr helm dep $@
+  $HELM_PLUGIN_DIR/cnr helm dep $@
 }
 
 function upgrade {
-    $HELM_PLUGIN_DIR/cnr helm upgrade $@
+  $HELM_PLUGIN_DIR/cnr helm upgrade $@
 }
 
 function cnr_helm {
-    $HELM_PLUGIN_DIR/cnr $@ --media-type=helm
+  $HELM_PLUGIN_DIR/cnr $@ --media-type=helm
 }
 
 
+download_or_noop
 case "$1" in
-    init-plugin)
-        update_cnr
-        ;;
-    install)
-        install "${@:2}"
-        ;;
-    upgrade)
-        upgrade "${@:2}"
-        ;;
-    dep)
-        dep "${@:2}"
-        ;;
-    pull)
-        pull "${@:2}"
-        ;;
-    push)
-        cnr_helm "$@"
-        ;;
-    list)
-        cnr_helm "$@"
-        ;;
-    show)
-        cnr_helm "$@"
-        ;;
-    delete-package)
-        cnr_helm "$@"
-        ;;
-    inspect)
-        cnr_helm "$@"
-        ;;
-    *)
-        $HELM_PLUGIN_DIR/cnr $@
-        ;;
-
+  init-plugin)
+    update_cnr
+    ;;
+  install)
+    install "${@:2}"
+    ;;
+  upgrade)
+    upgrade "${@:2}"
+    ;;
+  dep)
+    dep "${@:2}"
+    ;;
+  pull)
+    pull "${@:2}"
+    ;;
+  push)
+    cnr_helm "$@"
+    ;;
+  list)
+    cnr_helm "$@"
+    ;;
+  show)
+    cnr_helm "$@"
+    ;;
+  delete-package)
+    cnr_helm "$@"
+    ;;
+  inspect)
+    cnr_helm "$@"
+    ;;
+  *)
+    $HELM_PLUGIN_DIR/cnr $@
+    ;;
 esac
