65c4a83c07fb1bd018ccb577edd4be17e1b63476
Antoine Legrand <2t.antoine@gmail.com>
2017-09-05 15:11:05 +0200

    Release windows plugin

--- cnr.sh
+++ cnr.sh
@@ -46,28 +46,6 @@ function download_or_noop {
   fi
 }
 
-function pull {
-  #echo "pull $@"
-  release=$($HELM_PLUGIN_DIR/appr pull --media-type helm ${@} | tail -n1)
-  echo "$release"
-}
-
-function install {
-  $HELM_PLUGIN_DIR/appr helm install $@
-}
-
-function dep {
-  $HELM_PLUGIN_DIR/appr helm dep $@
-}
-
-function upgrade {
-  $HELM_PLUGIN_DIR/appr helm upgrade $@
-}
-
-function appr_helm {
-  $HELM_PLUGIN_DIR/appr $@ --media-type=helm
-}
-
 [ -z "$HELM_PLUGIN_DIR" ] && HELM_PLUGIN_DIR="$HOME/.helm/plugins/registry"
 LATEST=$(latest)
 download_or_noop $LATEST
@@ -79,34 +57,7 @@ case "$1" in
   list-plugin-versions)
     list_plugin_versions
     ;;
-  install)
-    install "${@:2}"
-    ;;
-  upgrade)
-    upgrade "${@:2}"
-    ;;
-  dep)
-    dep "${@:2}"
-    ;;
-  pull)
-    pull "${@:2}"
-    ;;
-  push)
-    appr_helm "$@"
-    ;;
-  list)
-    appr_helm "$@"
-    ;;
-  show)
-    appr_helm "$@"
-    ;;
-  delete-package)
-    appr_helm "$@"
-    ;;
-  inspect)
-    appr_helm "$@"
-    ;;
   *)
-    $HELM_PLUGIN_DIR/appr $@
+    $HELM_PLUGIN_DIR/appr helm $@
     ;;
 esac
